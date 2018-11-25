930f6b095659379f4473b89466cbbe9ed70b08b1
Yuri Egorov <ysegorov@gmail.com>
2018-11-25 16:41:16 +0300

    add postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,37 @@
+
+var debug = process.env.NODE_ENV === 'development';
+
+var plugins = [
+  require('postcss-import')({
+    path: [
+      'node_modules/sanitize.css/',
+      'node_modules/yagni.css/',
+    ]
+  }),
+  require('postcss-mixins')(),
+  require('postcss-css-variables')({
+    preserve: false
+  }),
+  require('postcss-color-hwb')(),
+  require('postcss-color-function')(),
+  require('postcss-flexbugs-fixes')(),
+  require('autoprefixer')()
+];
+
+if (!debug) {
+  plugins.push(require('cssnano')({
+      normalizeUrl: false
+    })
+  );
+} else {
+  plugins.push(require('postcss-discard-comments')());
+}
+
+plugins.push(require('postcss-reporter')());
+
+module.exports = function () {
+  return {
+    plugins: plugins,
+    map: false
+  };
+};
